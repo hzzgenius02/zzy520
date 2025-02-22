852a4ec8de0187cbcb54de8c7642fb744031d003
Huang zhou <62993512+hzzgenius02@users.noreply.github.com>
2025-02-23 00:29:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,15 +38,15 @@ noButton.addEventListener("click", function () {
 
     // 图片变化（前 5 次变化）
     // 震惊 修改为绝对路径
-    if (clickCount === 1) mainImage.src = "https://wx3.sinaimg.cn/orj360/005L0HLIgy1gqdiafl1rgg304q036q3d.gif";  
+    if (clickCount === 1) mainImage.src = "./images/2.gif";  
     // 思考 修改为绝对路径  
-    if (clickCount === 2) mainImage.src = "https://wx2.sinaimg.cn/orj360/0088mgqDgy1h0gi27qu9qg3035035761.gif";  
+    if (clickCount === 2) mainImage.src = "./images/3.gif";  
     // 生气 修改为绝对路径  
-    if (clickCount === 3) mainImage.src = "https://wx1.sinaimg.cn/orj360/0088mgqDgy1h0gi28kxymg3035035mym.gif"; 
+    if (clickCount === 3) mainImage.src = "./images/4.gif"; 
     // 哭 修改为绝对路径    
-    if (clickCount === 4) mainImage.src = "https://wx1.sinaimg.cn/orj360/0088mgqDgy1h0gi22yq44g3035035wf5.gif";   
+    if (clickCount === 4) mainImage.src = "./images/5.gif";   
     // 之后一直是哭
-    if (clickCount >= 5) mainImage.src = "https://wx1.sinaimg.cn/orj360/0088mgqDgy1h0gi22yq44g3035035wf5.gif";
+    if (clickCount >= 5) mainImage.src = "./images/5.gif";
     // if (clickCount === 1) mainImage.src = "https://pic.rmb.bdstatic.com/bjh/3ed189b421bd/250216/8e2317bb70ec0beeb30f4522cac1e915.png"; // 震惊
     // if (clickCount === 2) mainImage.src = "https://pic.rmb.bdstatic.com/bjh/3ed189b421bd/250216/85e54dee5f519ff1a2b8aa7e370db816.png";// 思考   
     // if (clickCount === 3) mainImage.src = "https://pic.rmb.bdstatic.com/bjh/3ed189b421bd/250216/74c1e4c813a3fc129c9bea588d00c4d7.png";   // 生气
@@ -60,7 +60,7 @@ yesButton.addEventListener("click", function () {
     document.body.innerHTML = `
         <div class="yes-screen">
             <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
-            <img src="https://wx4.sinaimg.cn/orj360/0088mgqDgy1h0gi24qy9sg3035035abh.gif" alt="拥抱" class="yes-image">   
+            <img src="./images/6.gif" alt="拥抱" class="yes-image">   
         </div>
     `;
 
